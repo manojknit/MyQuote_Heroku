53536429510c55b94c456a344241d8486e2bbe9d
Manoj Kumar <erpatel@gmail.com>
2017-11-12 03:08:50 +0000

    root

---
+++ public/app/app.js
@@ -0,0 +1,13 @@
+// angular.module('app', []);
+
+// angular.module('app').controller('testCtrl', function($scope){
+//     $scope.test = "working!";
+// });
+
+angular.module('app', []);
+
+angular.module('app').controller('testCtrl', function($scope) {
+    $scope.jobs = [{ title: 'Sales Person', description: 'you will fight dragons' },
+        { title: 'Accountant', description: 'you will use keyboard' }
+    ]
+});
