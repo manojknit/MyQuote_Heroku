87b6c6df08b4ced861b26b878908d9a6e91d7726
Manoj Kumar <erpatel@gmail.com>
2017-11-12 07:12:39 +0000

    mongodb

--- public/app/app.js
+++ public/app/app.js
@@ -4,10 +4,11 @@
 //     $scope.test = "working!";
 // });
 
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope) {
-    $scope.jobs = [{ title: 'Sales Person', description: 'you will fight dragons' },
-        { title: 'Accountant', description: 'you will use keyboard' }
-    ]
+angular.module('app').controller('testCtrl', function($scope, $resource) {
+    // $scope.jobs = [{ title: 'Sales Person', description: 'you will fight dragons' },
+    //     { title: 'Accountant', description: 'you will use keyboard' }
+    // ]
+    $scope.jobs = $resource('/api/jobs').query();
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var jobModel1 = require('./models/Job');
+
 var app = express();
 app.set('views', __dirname);
 app.set('view engine', 'jade');
@@ -7,17 +9,23 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname+'/public'));
 
 app.get('/api/jobs', function(req, res){
-    res.send('test');
+    //res.send('test');
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    })
 })
 
 app.get('*', function(req, res){
     res.render('index');
 })
-mongoose.connect('mongodb://localhost/jobfinder');
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://quoteuser:Monday1$@ds259105.mlab.com:59105/quotedb');
 
 var con = mongoose.connection;
 con.once('open', function(){
-    console.log('connection to mongodb successfully!')
+    console.log('connection to mongodb successfully!');
+    jobModel1.seedJobs();
 });
 
 app.listen(process.env.PORT,process.env.IP);
