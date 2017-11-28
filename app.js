70d05bd315b3276967523e0e579ac54cbf49699b
Manoj Kumar <erpatel@gmail.com>
2017-11-27 19:18:30 -0800

    server

--- app.js
+++ app.js
@@ -83,10 +83,10 @@ process.on('SIGINT', function() {
   }); 
 }); 
 
-app.listen(process.env.PORT,process.env.IP);
+//app.listen(process.env.PORT,process.env.IP);
 //app.listen(process.env.PORT||3000);
-// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-// var server = app.listen(app.get('port'), function() {
-//   console.log('Express server listening on port ' + server.address().port);
-// });
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
